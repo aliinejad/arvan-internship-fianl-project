2b94d6a9e9f83bfe3c5612d409900dfe652fc42a
aliinejad <m.alinejad.h@gmail.com>
2021-05-05 08:16:02 +0000

    add json

--- main.py
+++ main.py
@@ -7,21 +7,25 @@ def root():
 
 
 @app.route('/add',methods=['POST'])
-def asgher():
+def add():
 
 	data=request.get_json()
+	input_port = data['input_port']
+	upstream = data['upstream']+ ":" + data['upstream_port']
+	path = "/etc/nginx/stream/" + input_port + ".conf"
+	
 	c = nginx.Conf()
 	s = nginx.Server()
 
 	s.add(
-     		nginx.Key('listen', '12346'),
-     		nginx.Comment('Yes, python-nginx can read/write comments!'),
-     		nginx.Key('proxy_pass', '194.5.192.202:22'),
+     		nginx.Key('listen', input_port),
+     		nginx.Key('proxy_pass', upstream )
 		#     nginx.Key('proxy_protocol', 'on'),
 		#     nginx.Key('set_real_ip_from', '$proxy_protocol_addr')
  		)
 	c.add(s)
-	nginx.dumpf(c, '/etc/nginx/stream/mysite1.conf')
+	nginx.dumpf(c, path )
+	return "Done!"
 	
 
 
