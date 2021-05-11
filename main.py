b552e9d27dce5968688ca35a0f285c1b8fe526b2
aliinejad <m.alinejad.h@gmail.com>
2021-05-11 07:33:55 +0000

    add reload.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import nginx
+import requests
 import os
 from flask import Flask, jsonify, request
 app = Flask(__name__, static_folder='.', static_url_path='')
@@ -26,7 +27,8 @@ def add():
 	c.add(s)
 	nginx.dumpf(c, path )
 	os.system("/etc/init.d/nginx reload")
-	return "Done!"
+	return requests.get('http://194.5.193.18:5000/').content
+#	return "Done!"
 	
 
 
---
+++ reload.py
@@ -0,0 +1,19 @@
+import inotify.adapters
+import os
+
+def _main():
+    i = inotify.adapters.Inotify()
+
+    i.add_watch('/etc/nginx/stream/')
+    for event in i.event_gen(yield_nones=False):
+        (_, type_names, path, filename) = event
+	
+	if type_names[0] == 'IN_CREATE':
+		print(type_names)
+		os.system("/etc/init.d/nginx reload")
+      #  print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(
+       #       path, filename, type_names))
+
+if __name__ == '__main__':
+    _main()
+
