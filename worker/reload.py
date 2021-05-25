88c19da1d42093cd1c4883f7d066a77ef2f920fc
aliinejad <m.alinejad.h@gmail.com>
2021-05-25 08:24:30 +0000

    add docker image for workers

--- worker/reload.py
+++ worker/reload.py
@@ -6,11 +6,10 @@ def _main():
 
     i.add_watch('/etc/nginx/stream/')
     for event in i.event_gen(yield_nones=False):
-        (_, type_names, path, filename) = event
-	
-	if type_names[0] == 'IN_CREATE':
-		print(type_names)
-		os.system("/etc/init.d/nginx reload")
+        (_, type_names, path, filename) = event	
+        if type_names[0] == 'IN_CREATE':
+                    print(type_names)
+                    os.system("/etc/init.d/nginx reload")
 
 if __name__ == '__main__':
     _main()
---
+++ worker/req/script.sh
@@ -0,0 +1,4 @@
+
+/etc/init.d/nginx restart
+python3 /code/reload.py
+
